604c9ce87b225780b1a0946a4019efb49ce76a91
Geneveve <115051918+genrp24@users.noreply.github.com>
2023-03-01 21:26:51 -0600

    Added password promts to JS file. Also added in README template in repo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  var pwdLength = prompt('How many characters would you like the password to be?')
+    console.log(pwdLength);
+  var pwdLowerCase = prompt('Would you like to include lowercase letters?')
+    console.log(pwdLowerCase);
+  var pwdUpperCase = prompt('Would you like to include uppercase letters?')
+    console.log(pwdUpperCase);
+  var pwdNumeric = prompt('Would you like to include numeric characters?')
+    console.log(pwdNumeric);
+  var pwdSpecial = prompt('Would you like to include special characters?')
+    console.log(pwdSpecial);
+};
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
